b423f64c9c4bd78f4d86939bb3dcf1e1bea266ab
GeeeekExplorer <2651904866@qq.com>
2023-02-27 00:30:50 +0800

    finish hw02

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -5,29 +5,27 @@
 struct Node {
     // 这两个指针会造成什么问题？请修复
     std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    std::weak_ptr<Node> prev;
     // 如果能改成 unique_ptr 就更好了!
 
     int value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
-    }
+    Node(int val): value(val) {}
 
-    void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
-    }
+    // void insert(int val) {
+    //     auto node = std::make_shared<Node>(val);
+    //     node->next = next;
+    //     node->prev = prev;
+    //     if (prev)
+    //         prev->next = node;
+    //     if (next)
+    //         next->prev = node;
+    // }
 
     void erase() {
-        if (prev)
-            prev->next = next;
+        if (auto p = prev.lock())
+            p->next = next;
         if (next)
             next->prev = prev;
     }
@@ -44,8 +42,14 @@ struct List {
 
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        // head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
+        head = std::make_shared<Node>(other.head->value);
+        for (auto curr = other.head->next, tail = head; curr; curr = curr->next, tail = tail->next) {
+            auto node = std::make_shared<Node>(curr->value);
+            node->prev = tail;
+            tail->next = node;
+        }
     }
 
     List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
@@ -80,7 +84,7 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List &lst) {  // 有什么值得改进的？
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
